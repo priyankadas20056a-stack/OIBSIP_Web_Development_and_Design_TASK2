561100d27c85c5803ef63fd8c4b8acde11e859d9
PRIYANKA DAS <priyankadas20056a@gmail.com>
2025-09-05 03:16:51 +0530

    Add files via upload

---
+++ issue.php
@@ -0,0 +1,8 @@
+<?php
+ 
+ 
+    echo " COMPANY NAME IS :- " . $_POST['name']."<br>";
+    echo " COMPANY'S EMAIL IS :- ".$_POST['email']."<br>";
+    echo " COMPANY PHONE NUMBER IS :- " . $_POST['number']."<br>";
+    echo " THE PROBLEM WHICH HE/SHE FACES  :- ".$_POST['issue']."<br>";
+     ?>
